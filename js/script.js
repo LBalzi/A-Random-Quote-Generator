6275ea6fb6fb9f416591a10c843da1d391406d63
LBalzi <149258047+LBalzi@users.noreply.github.com>
2023-10-28 23:56:04 +0200

    finished up

--- js/script.js
+++ js/script.js
@@ -3,19 +3,15 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /*** 
- * `quotes` array 
+ * `quotes` array with ten quotes, some have citation and/or year and some don't
 ***/
 
 const quotes = [ 
   {"quote": "You may say I'm a dreamer, but I'm not the only one",
   "source": "John Lennon",
   "citation": "Imagine",
-  "year": "1971"
+  "year": ""
   },
   {"quote": "I believe I can fly, I believe I can touch the sky",
   "source": "R. Kelly",
@@ -30,7 +26,7 @@ const quotes = [
   {"quote": "You're beautiful, it's true",
   "source": "James Blunt",
   "citation": "You're Beautiful",
-  "year": "2004"
+  "year": ""
   },
   {"quote": "I'm starting with the man in the mirror. I'm asking him to change his ways",
   "source": "Michael Jackson",
@@ -39,17 +35,17 @@ const quotes = [
   },
   {"quote": "Cause you're a sky, 'cause you're a sky full of stars. I'm gonna give you my heart",
   "source": "Coldplay",
-  "citation": "A Sky Full of Stars",
+  "citation": "",
   "year": "2014"
   },
   {"quote": "You can't always get what you want, but if you try sometimes, well, you might find, you get what you need",
   "source": "The Rolling Stones",
   "citation": "You Can't Always Get What You Want",
-  "year": "1969"
+  "year": ""
   },
   {"quote": "I've paid my dues, time after time. I've done my sentence but committed no crime",
   "source": "Queen",
-  "citation": "We Are the Champions",
+  "citation": "",
   "year": "1977"
   },
   {"quote": "And in the end, the love you take is equal to the love you make",
@@ -59,14 +55,15 @@ const quotes = [
   },
   {"quote": "I wanna dance with somebody, I wanna feel the heat with somebody",
   "source": "Whitney Houston",
-  "citation": "I Wanna Dance with Somebody",
+  "citation": "",
   "year": "1987"
   }
 ];
 
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function, it takes a random number from 1 to the length of the objects array above
+ *  and uses it to return a random quote 
 ***/
 
 function getRandomQuote() {
@@ -75,22 +72,30 @@ function getRandomQuote() {
 }
 
 /***
- * `printQuote` function
+ * `printQuote` function stores the random quote object in the variable randomQuote and themn builds the html required
+ * to show the quote on the page. Finally it takes the html snippet it wrote and injects it in the 'quote-box'
 ***/
 
 function printQuote() {
   let randomQuote = getRandomQuote(quotes);
-  let html = `<p class="quote">${randomQuote.quote}</p>
-  <p class="source">${randomQuote.source}</p>
-  <p class="citation">${randomQuote.citation}</p>
-  <p class="year">${randomQuote.year}</p>
+  let html = 
+  `<p class="quote">${randomQuote.quote}</p>
+  <p class="source">${randomQuote.source}
   `;
-
+  if (randomQuote.source !== "") {  
+    html += `<span class="citation">${randomQuote.citation}</span>`;
+  };
+  if (randomQuote.year !== "") {
+    html += `<span class="year">${randomQuote.year}</span>`
+  };
+  html+= `</p>`
+  document.getElementById('quote-box').innerHTML = html; 
 };
+
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-//document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
